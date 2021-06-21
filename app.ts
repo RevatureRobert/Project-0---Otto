f05fa21063686323fa5e03f31d1fa9c7fe938bb2
Matthew Otto <otto.a.matthew@gmail.com>
2021-06-21 15:25:13 -0500

    Documentation/Comments

--- app.ts
+++ app.ts
@@ -3,14 +3,14 @@ import totalRouter from './routes/vortex';
 const app = express();
 
 /**
- * 
+ * './api' to declare the api version of my site, then proceeds to the Routes folder.
  */
 app.use(express.json())
 app.use('/api', totalRouter);
 
 
 /**
- * 
+ * Listen declares the port 4000.
  */
 app.listen(4000, () => {
     console.log('Listening on port 4000...')
--- routes/vortex.ts
+++ routes/vortex.ts
@@ -5,7 +5,13 @@ const buildRoute = new Router();
 
 
 /**
- * 
+ * Break out the HTTP into individual Objects and Functions.
+ * '/' is the entire collection.
+ * '/genre' is to get games by the Genre of the game.
+ * '/title' is an advanced search for specific titles per genre.
+ * '/add' can add a game or update one.
+ * '/delete' will remove a game from the table dependant on both genre and id.
+ * Progress to 'leylines.ts'.
  */
 buildRoute.get('/', getAllGames);
 buildRoute.get('/:genre', getByGenreGames);
@@ -15,7 +21,7 @@ buildRoute.delete('/delete/:genre/:id', delGames);
 
 
 /**
- * 
+ * Using './games', this new Router object condenses the HTTP Requests into a single object.
  */
 const totalRouter = Router();
 totalRouter.use('/games', buildRoute);
